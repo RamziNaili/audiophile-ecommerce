8062a4e7945eb05fca62d75d40cee26d0b1ad9eb
RamziNaili <naili.ramzi@outlook.fr>
2024-09-03 13:30:44 +0200

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#D87D4A',
+        secondary: '#101010',
+        gray: '#F1F1F1',
+        light_gray: '#FAFAFA',
+        light_orange: '#fbaf85',
+      },
+    },
   },
   plugins: [],
 };
