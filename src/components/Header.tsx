148b477f9a02b5cce666468204c113a824518a65
Ramzi NAILI <ranaili@paul-f4Br6s1.clusters.42paris.fr>
2024-09-04 16:34:29 +0200

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,30 @@
 import { FC } from "react";
+import Logo from "../assets/shared/desktop/logo.svg?react";
+import Cart from "../assets/shared/desktop/icon-cart.svg?react";
+import BurgerMenu from "../assets/shared/tablet/icon-hamburger.svg?react";
 
 export const Header: FC = () => {
-  return <div></div>;
+  return (
+    <section className="flex justify-center bg-black h-24">
+      <div className="flex items-center justify-between w-3/4 border-b">
+        <div className="flex items-center gap-10">
+          <BurgerMenu className="md:hidden" />
+          <Logo className="cursor-pointer" />
+        </div>
+        <nav className="flex text-white uppercase lg:gap-8 gap-3 max-md:hidden">
+          <p className="hover:text-primary font-bold cursor-pointer">home</p>
+          <p className="hover:text-primary font-bold cursor-pointer">
+            headphones
+          </p>
+          <p className="hover:text-primary font-bold cursor-pointer">
+            speakers
+          </p>
+          <p className="hover:text-primary font-bold cursor-pointer">
+            earphones
+          </p>
+        </nav>
+        <Cart className="cursor-pointer" />
+      </div>
+    </section>
+  );
 };
