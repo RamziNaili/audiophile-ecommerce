e9a0b44aa3dbaf2e16d3156a002adffc35dc7fb1
Ramzi NAILI <ranaili@paul-f4Br6s1.clusters.42paris.fr>
2024-09-04 17:13:01 +0200

    routing and pages

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import Logo from "../assets/shared/desktop/logo.svg?react";
 import Cart from "../assets/shared/desktop/icon-cart.svg?react";
 import BurgerMenu from "../assets/shared/tablet/icon-hamburger.svg?react";
+import { NavLink } from "react-router-dom";
 
 export const Header: FC = () => {
   return (
@@ -9,19 +10,33 @@ export const Header: FC = () => {
       <div className="flex items-center justify-between w-3/4 border-b">
         <div className="flex items-center gap-10">
           <BurgerMenu className="md:hidden" />
-          <Logo className="cursor-pointer" />
+          <Logo />
         </div>
         <nav className="flex text-white uppercase lg:gap-8 gap-3 max-md:hidden">
-          <p className="hover:text-primary font-bold cursor-pointer">home</p>
-          <p className="hover:text-primary font-bold cursor-pointer">
+          <NavLink
+            to=""
+            className="hover:text-primary font-bold cursor-pointer"
+          >
+            home
+          </NavLink>
+          <NavLink
+            to="/headphones"
+            className="hover:text-primary font-bold cursor-pointer"
+          >
             headphones
-          </p>
-          <p className="hover:text-primary font-bold cursor-pointer">
+          </NavLink>
+          <NavLink
+            to="/speakers"
+            className="hover:text-primary font-bold cursor-pointer"
+          >
             speakers
-          </p>
-          <p className="hover:text-primary font-bold cursor-pointer">
+          </NavLink>
+          <NavLink
+            to="/earphones"
+            className="hover:text-primary font-bold cursor-pointer"
+          >
             earphones
-          </p>
+          </NavLink>
         </nav>
         <Cart className="cursor-pointer" />
       </div>
---
+++ src/pages/Headphones.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const Headphones: FC = () => {
+  return <div>headphone</div>;
+};
