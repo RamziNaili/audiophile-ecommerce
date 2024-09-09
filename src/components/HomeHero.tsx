4aa662abdb2afd8fb77161433d56a086012dd6b7
Ramzi NAILI <ranaili@paul-f4Br8s1.clusters.42paris.fr>
2024-09-09 11:38:26 +0200

    home page : HomeHero done

--- src/components/HomeHero.tsx
+++ src/components/HomeHero.tsx
@@ -1,18 +1,22 @@
-import { FC } from 'react';
-import { Header } from './Header';
-import { Button } from './Button';
+import { FC } from "react";
+import { Header } from "./Header";
+import { Button } from "./Button";
 
 export const HomeHero: FC = () => {
   return (
-    <section className="flex justify-center bg-[url('src/assets/home/desktop/image-hero.jpg')] ">
+    <section
+      className={`flex justify-center bg-[url('src/assets/home/mobile/image-header.jpg')]
+        sm:bg-[url('src/assets/home/tablet/image-header.jpg')]
+        md:bg-[url('src/assets/home/desktop/image-hero.jpg')] bg-no-repeat bg-cover w-full`}
+    >
       <div className="h-[729px] w-3/4">
         <Header />
-        <div className="mt-32">
+        <div className="mt-32 flex flex-col items-center md:items-start">
           <p className="text-white/[.4964] tracking-[10px] ">NEW PRODUCT</p>
-          <h1 className="text-white text-[56px] uppercase font-bold leading-[58px] w-[400px] mt-6">
+          <h1 className="text-white text-[36px] sm:text-[56px] uppercase font-bold leading-[58px] w-[328px] sm:w-[400px] mt-6 max-sm:text-center">
             XX99 Mark II Headphones
           </h1>
-          <p className="text-white/[.75] w-[350px] mt-6 leading-[25px] text-[15px]">
+          <p className="text-white/[.75] w-[328px] sm:w-[350px] mt-6 leading-[25px] text-[15px] text-center md:text-left">
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
           </p>
