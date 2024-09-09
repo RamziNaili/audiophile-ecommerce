46243f68d34206a2abe96e1d7ae100a16cd62f4c
Ramzi NAILI <ranaili@paul-f4Br8s1.clusters.42paris.fr>
2024-09-09 14:59:01 +0200

    home Page : bestseller mobile done

--- src/components/HomeBestsellers.tsx
+++ src/components/HomeBestsellers.tsx
@@ -1,9 +1,9 @@
 import { FC } from "react";
-import Circle from "../assets/home/desktop/pattern-circles.svg?react";
+import { Button } from "./Button";
 
 export const HomeBestsellers: FC = () => {
   return (
-    <section className="flex justify-center mt-32 md:mt-24 xl:mt-40 mb-96 ">
+    <section className="flex flex-col items-center mt-32 md:mt-24 xl:mt-40 mb-96 ">
       <div className="bg-primary w-4/5 h-[600px] rounded-[8px] flex flex-col items-center">
         {/* <Circle /> */}
         <img
@@ -14,10 +14,32 @@ export const HomeBestsellers: FC = () => {
         <h2 className="mt-8 text-white font-bold text-[36px] tracking-[1.29px] w-3/5 text-center leading-[40px]">
           ZX9 SPEAKER
         </h2>
-        <p className="font-medium text-[15px] mt-[24px] text-white text-center w-4/5 leading-[25px]">
+        <p className="font-medium text-[15px] mt-[24px] text-white/[.75] text-center w-4/5 leading-[25px]">
           Upgrade to premium speakers that are phenomenally built to deliver
           truly remarkable sound.
         </p>
+        <Button className="mt-6" variant="outlineReverse">
+          see product
+        </Button>
+      </div>
+      <div className="bg-[url('src/assets/home/mobile/image-speaker-zx7.jpg')] rounded-[8px] mt-6 w-4/5 h-[330px] pl-6 flex flex-col justify-center gap-8 bg-cover bg-no-repeat">
+        <h2 className="font-bold text-[28px] tracking-[2px]">ZX7 SPEAKER</h2>
+        <Button variant="outline" className="w-fit">
+          see product
+        </Button>
+      </div>
+      <img
+        src="src/assets/home/mobile/image-earphones-yx1.jpg"
+        alt="earphones-yx1"
+        className="w-4/5 mt-6 rounded-[8px]"
+      />
+      <div className="mt-6 bg-gray pl-6 w-4/5 rounded-[8px]">
+        <h2 className="pt-10 font-bold text-[28px] tracking-[2px]">
+          YX1 EARPHONES
+        </h2>
+        <Button className="mt-8 mb-10" variant="outline">
+          see product
+        </Button>
       </div>
     </section>
   );
