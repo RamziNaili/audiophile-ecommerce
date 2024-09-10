10f66c674b1ea8f896fe41c0b44a2a1f6b971347
RamziNaili <naili.ramzi@outlook.fr>
2024-09-10 14:02:03 +0200

    home page Footer done

--- src/components/UI/Layout.tsx
+++ src/components/UI/Layout.tsx
@@ -1,12 +1,14 @@
-import { FC } from "react";
-import { Outlet } from "react-router-dom";
+import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Footer } from './Footer';
 
 export const Layout: FC = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <div className="flex-grow">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
