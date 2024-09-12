06b27f8275c6726a89ce6051738143cf8505f4d3
Ramzi NAILI <ranaili@paul-f4Br7s1.clusters.42paris.fr>
2024-09-12 11:46:31 +0200

    productInfos done

--- src/components/UI/ProductInfos.tsx
+++ src/components/UI/ProductInfos.tsx
@@ -10,71 +10,77 @@ type Props = {
 export const ProductInfos: FC<Props> = ({ product }) => {
   return (
     <section className="flex flex-col justify-center">
-      <div
-        className={`flex flex-col lg:flex-row lg:gap-[125px] justify-center items-center lg:w-3/4 `}
-      >
-        <div className="flex flex-col items-center justify-center">
-          <p className="max-lg:w-5/6 font-medium text-black/[.5] text-[15px] mb-6">
-            Go Back
-          </p>
-          <img
-            src={"src/" + product?.image.mobile}
-            alt={product?.slug}
-            className="max-lg:w-5/6 rounded-[8px]"
-          />
-        </div>
-
-        <div className="flex flex-col justify-center max-md:w-5/6">
-          {product?.new && (
-            <p className="mt-8 sm:mt-12 uppercase text-[14px] tracking-[10px] text-primary">
-              new product
+      <div className="flex justify-center">
+        <div
+          className={`flex flex-col lg:flex-row lg:gap-[125px] justify-around items-center lg:w-3/4 `}
+        >
+          <div className="flex flex-col max-lg:items-center justify-center">
+            <p className="max-lg:w-5/6 font-medium text-black/[.5] text-[15px] mb-6">
+              Go Back
             </p>
-          )}
-          <h4
-            className={`font-bold text-[28px] tracking-[1px] sm:tracking-[1.43px] sm:leading-[44px] sm:text-[40px] uppercase w-3/4 lg:w-[400px] ${
-              product?.new ? "mt-6" : "mt-8 sm:mt-12"
-            }`}
-          >
-            {product?.name}
-          </h4>
-          <p className="w-3/4 lg:w-[450px] text-black/[.5] font-medium text-[15px] leading-[25px] mt-6 sm:mt-8">
-            {product?.description}
-          </p>
-          <p className="mt-6 font-bold text-[18px] tracking-[1.29px]">
-            {"$ " + product?.price}
-          </p>
-          <div className="flex justify-between mt-7">
-            <Numbers />
-            <Button>add to cart</Button>
+            <img
+              src={"src/" + product?.image.mobile}
+              alt={product?.slug}
+              className="w-5/6 lg:w-[550px] rounded-[8px]"
+            />
+          </div>
+
+          <div className="flex flex-col justify-center max-md:w-5/6 ">
+            {product?.new && (
+              <p className="mt-8 sm:mt-12 uppercase text-[14px] tracking-[10px] text-primary">
+                new product
+              </p>
+            )}
+            <h4
+              className={`font-bold text-[28px] tracking-[1px] sm:tracking-[1.43px] sm:leading-[44px] sm:text-[40px] uppercase w-3/4 lg:w-[400px] ${
+                product?.new ? "mt-6" : "mt-8 sm:mt-12"
+              }`}
+            >
+              {product?.name}
+            </h4>
+            <p className="w-3/4 lg:w-[450px] text-black/[.5] font-medium text-[15px] leading-[25px] mt-6 sm:mt-8">
+              {product?.description}
+            </p>
+            <p className="mt-6 font-bold text-[18px] tracking-[1.29px]">
+              {"$ " + product?.price}
+            </p>
+            <div className="flex gap-4 mt-7">
+              <Numbers />
+              <Button>add to cart</Button>
+            </div>
           </div>
         </div>
       </div>
-      <div className="mt-20 flex justify-center">
-        <div className="w-5/6">
-          <p className="uppercase font-bold text-[24px] leading-[36px] tracking-[0.86px]">
-            features
-          </p>
-          <p className="mt-6 font-medium text-black/[.5] text-[15px] leading-[25px]">
-            {product?.features}
-          </p>
-        </div>
-      </div>
-      <div className="mt-20 flex justify-center">
-        <div className="w-5/6">
-          <p className="uppercase font-bold text-[24px] leading-[36px] tracking-[.86px] mb-6">
-            in the box
-          </p>
-          <div className="flex flex-col gap-2">
-            {product?.includes.map((p) => (
-              <div className="flex gap-[21px]">
-                <p className="text-primary font-bold text-[15px] leading-[25px] ">
-                  {p.quantity}x
-                </p>
-                <p className="text-black/[.5] font-medium text-[15px] leading-[25px]">
-                  {p.item}
-                </p>
+      <div className="md:flex md:justify-center">
+        <div className="md:flex md:flex-row md:gap-[125px] md:w-3/4">
+          <div className="mt-20 flex justify-center">
+            <div className="max-md:w-5/6">
+              <p className="uppercase font-bold text-[24px] leading-[36px] tracking-[0.86px]">
+                features
+              </p>
+              <p className="mt-6 font-medium text-black/[.5] text-[15px] leading-[25px]">
+                {product?.features}
+              </p>
+            </div>
+          </div>
+          <div className="mt-20 flex justify-center">
+            <div className="w-5/6 md:w-[230px] flex flex-col sm:flex-row sm:gap-[168px] md:flex-col md:gap-0">
+              <p className="uppercase font-bold text-[24px] leading-[36px] tracking-[.86px] mb-6">
+                in the box
+              </p>
+              <div className="flex flex-col gap-2">
+                {product?.includes.map((p) => (
+                  <div className="flex gap-[21px]">
+                    <p className="text-primary font-bold text-[15px] leading-[25px] ">
+                      {p.quantity}x
+                    </p>
+                    <p className="text-black/[.5] font-medium text-[15px] leading-[25px]">
+                      {p.item}
+                    </p>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
         </div>
       </div>
