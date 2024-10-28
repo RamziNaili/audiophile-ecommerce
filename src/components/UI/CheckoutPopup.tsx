be4569a788972b7b0a4d93caa9846e7ce648df93
RamziNaili <naili.ramzi@outlook.fr>
2024-10-28 11:36:04 +0100

    change img import

--- src/components/UI/CheckoutPopup.tsx
+++ src/components/UI/CheckoutPopup.tsx
@@ -4,12 +4,12 @@ import { NavLink } from 'react-router-dom';
 import { Button } from '../Button';
 import { Numbers } from '../Numbers';
 
-import xx59Headphones from 'src/assets/cart/image-xx59-headphones.jpg';
-import xx99MarkOneHeadphones from 'src/assets/cart/image-xx99-mark-one-headphones.jpg';
-import xx99MarkTwoHeadphones from 'src/assets/cart/image-xx99-mark-two-headphones.jpg';
-import yx1Earphones from 'src/assets/cart/image-yx1-earphones.jpg';
-import zx7Speaker from 'src/assets/cart/image-zx7-speaker.jpg';
-import zx9Speaker from 'src/assets/cart/image-zx9-speaker.jpg';
+import xx59Headphones from '../assets/cart/image-xx59-headphones.jpg';
+import xx99MarkOneHeadphones from '../assets/cart/image-xx99-mark-one-headphones.jpg';
+import xx99MarkTwoHeadphones from '../assets/cart/image-xx99-mark-two-headphones.jpg';
+import yx1Earphones from '../assets/cart/image-yx1-earphones.jpg';
+import zx7Speaker from '../assets/cart/image-zx7-speaker.jpg';
+import zx9Speaker from '../assets/cart/image-zx9-speaker.jpg';
 
 export const CheckoutPopup: FC = () => {
   const { cartItems, totalItems, removeAll, totalPrice } = useCartStore(
