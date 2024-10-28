b21c73e187b4781839cafee58aec172424433ebe
RamziNaili <naili.ramzi@outlook.fr>
2024-10-28 11:56:35 +0100

    change img import

--- src/components/UI/ProductDescription.tsx
+++ src/components/UI/ProductDescription.tsx
@@ -1,7 +1,14 @@
-import { FC } from "react";
-import { Product } from "../../types/types";
-import { Button } from "../Button";
-import { NavLink } from "react-router-dom";
+import { FC } from 'react';
+import { Product } from '../../types/types';
+import { Button } from '../Button';
+import { NavLink } from 'react-router-dom';
+
+import yx1EarphonesMobile from '../../assets/product-yx1-earphones/mobile/image-product.jpg';
+import xx59HeadphonesMobile from '../../assets/product-xx59-headphones/mobile/image-product.jpg';
+import xx99MarkOneHeadphonesMobile from '../../assets/product-xx99-mark-one-headphones/mobile/image-product.jpg';
+import xx99MarkTwoHeadphonesMobile from '../../assets/product-xx99-mark-two-headphones/mobile/image-product.jpg';
+import zx7SpeakerMobile from '../../assets/product-zx7-speaker/mobile/image-product.jpg';
+import zx9SpeakerMobile from '../../assets/product-zx9-speaker/mobile/image-product.jpg';
 
 type Props = {
   product: Product;
@@ -11,14 +18,23 @@ type Props = {
 export const ProductDescription: FC<Props> = ({ product, index }) => {
   const isEvenIndex = index % 2 === 0;
 
+  const images = [
+    yx1EarphonesMobile,
+    xx59HeadphonesMobile,
+    xx99MarkOneHeadphonesMobile,
+    xx99MarkTwoHeadphonesMobile,
+    zx7SpeakerMobile,
+    zx9SpeakerMobile,
+  ];
+
   return (
     <div className="flex justify-center">
       <div
-        className={`flex flex-col lg:flex-row lg:gap-[125px] justify-center mb-32 lg:w-3/4 ${!isEvenIndex && "lg:flex-row-reverse"}`}
+        className={`flex flex-col lg:flex-row lg:gap-[125px] justify-center mb-32 lg:w-3/4 ${!isEvenIndex && 'lg:flex-row-reverse'}`}
       >
         <div className="flex items-center justify-center">
           <img
-            src={"src/" + product.image.mobile}
+            src={images[product.id - 1]}
             alt={product.slug}
             className="max-lg:w-5/6 rounded-[8px]"
           />
@@ -32,7 +48,7 @@ export const ProductDescription: FC<Props> = ({ product, index }) => {
           )}
           <h4
             className={`font-bold text-[28px] tracking-[1px] sm:tracking-[1.43px] sm:leading-[44px] max-lg:text-center sm:text-[40px] uppercase w-3/5 lg:w-[400px] ${
-              product.new ? "mt-6" : "mt-8 sm:mt-12"
+              product.new ? 'mt-6' : 'mt-8 sm:mt-12'
             }`}
           >
             {product.name}
